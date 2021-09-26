356e226f1e7d07ef08a22772a4d4f842bb766fd8
Kunszt Norbert <kunszt.norbert@gmail.com>
2021-09-26 15:45:45 +0200

    create front

---
+++ front/front.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script>
+
+    </script>
+</head>
+<body>
+<?php
+
+$dir = plugin_dir_path(__DIR__).'db'.DIRECTORY_SEPARATOR.'connect.php';
+require_once $dir;
+
+
+$c = new Connect();
+
+$all = $c->getAllRoom();
+var_dump($all)
+
+?>
+
+
+</body>
+</html>
--- jegyzet.php
+++ jegyzet.php
@@ -19,3 +19,40 @@ function lista_button()
         'dashicons-welcome-widgets-menus'
     );
 }
+
+class Connect  {
+    private $pdo;
+    private $result;
+    private $caller;
+
+    public function __construct()
+    {
+        $user = "root";
+        $db_name = 'szobekwe_wp1';
+        $db_pass = "";
+        $password = $db_pass;
+        $dsn = 'mysql: host=localhost; dbname=' . $db_name;
+        try {
+            $this->pdo = new PDO($dsn, $user, $password);
+            $this->pdo->exec("set names utf8");
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
+        }
+    }
+
+    private function makeCall($callString) {
+        $this->caller = $callString;
+        $res = $this->pdo->prepare();
+        $res->execute($this->caller);
+        while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+            $this->result[] = $row;
+        }
+
+        return $this->result;
+    }
+
+    public function getAllRoom() {
+        $this->makeCall("SELECT * FROM todo");
+    }
+}
